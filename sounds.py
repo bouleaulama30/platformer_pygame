98577dbbe64765f79d3c7a0a12817ac4c4f35124
lyla <demange.telecom@gmail.com>
2023-10-20 17:39:52 +0200

    quadrille du homard

--- sounds.py
+++ sounds.py
@@ -5,12 +5,17 @@ pygame.mixer.init()
 
 musics = {
         'welcome' : "sons/mysterious_alice_theme.mp3",
-        "quadrille" : "sons/rebond_troll.mp3",
+        "quadrille" : "sons/quadrille_des_homards.mp3",
         "explore" :"sons/welcome_to_wonderland.mp3",
         "end" : "sons/welcome_to_wonderland.mp3",
         'rebond': "sons/rebond_troll.mp3"
 }
 
+"""
+pour exporter sons youtube :
+1) https://notube.io/fr/youtube-app-v102
+2) https://clideo.com/fr/cut-audio
+"""
 sounds = {
         'rebond': pygame.mixer.Sound("sons/rebond_troll.mp3"),
         'jump': pygame.mixer.Sound("sons/jump_troll.mp3"),
